34a423fe8d1c3d03d08c83f7ef5173c1b55dbcf8
aliceheneghan <heneghan31@gmail.com>
2022-08-19 12:56:52 +0200

    added image, title and birthday messages

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+// Styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="photo">
+        <img src={require('./images/rain2.jpg')} alt="rain"/>
+      </div>
+      <section>
+        <div className='title'>
+          Happy Birthday Emil
+        </div>
+        <div className='messages'>
+          <h4>We heard you like the rain so we wrote you some birthday messages about it :)</h4>
+          <p>Erika -Happiness is ...the sound of raindrops on your umbrella. Wish you a great Birthday Emil!!! </p>
+          <p>Maykal - Не сердитесь на дождь, просто он не умеет идти вверх. Эмиль, с днем рождения, желаю тебе всего наилучшего!</p>
+          <p>Miguel - Feliz Cumpleaños Emil, I hope today it rains enough for you!!</p>
+          <p>Baha - Hey Emil, Happy Birthday Dude!! Have a nice year.</p>
+          <p>Lucia - Happy birthday Emil (ambient sound, rain in the background)</p>
+          <p>Justi - Raindrop drop-top, wishing Emil a happy birthday non-stop</p>
+          <p>Vincenzo - Happy Birthday Emil! May this day make good luck, success and happiness rain on you! "I'm singing in the rain..."</p>
+          <p>Daan - When coding causes pain to your brain, don’t complain, just get out in the rain. Happy Birthday Emil!</p>
+          <p>Aika - Желаю, чтобы в климатической зоне твоей судьбы господствовали циклоны здоровья, а осадки были только в виде дождей счастья, града удачи и сияющего инея любви! С днем рождения, Эмиль!</p>
+          <p>Nievin -happy birthday Emil </p>
+          <p>Naglaa - Happy Birthday Emil</p>
+          <p>Sangeetha - Happy Birthday Emil </p>
+          <p>Alice - maybe for your birthday, the rain can gift you with a rainbow. Enjoy your day!</p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
